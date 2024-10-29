eb8606c55c160f2a2357ae0df15065e107b6fe6e
elmaaref <abdelrahimabuelmaaref@gmail.com>
2024-10-30 00:56:35 +0300

    Product Collection Resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return new ProductCollection(Product::paginate());
+        return ProductCollection::collection(Product::paginate());
         // return Product::paginate();
     }
 
--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -3,9 +3,9 @@
 namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,16 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'totalPrice' => round((1 - $this->discount / 100) * $this->price, 2),
+            'discount' => $this->discount,
+            'rating' => $this->reviews->count() 
+                        ? round($this->reviews->sum('star') / $this->reviews->count(), 2) 
+                        : 'Non rating yet',
+            'link' => [
+                'href' => route('products.show', $this->id),
+            ]
+        ];
     }
 }
